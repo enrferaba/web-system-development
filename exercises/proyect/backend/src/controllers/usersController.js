795a330dd8b53d47989eb621e52aa63c5f8c982d
Enrique Feria Abascal <enfeaba@gmail.com>
2025-12-13 13:50:25 +0100

    Remove comments from usersController

--- exercises/proyect/backend/src/controllers/usersController.js
+++ exercises/proyect/backend/src/controllers/usersController.js
@@ -1,4 +1,3 @@
-// src/controllers/usersController.js
 import userModel from "../models/userModel.js";
 import { userSchema } from "../db/userSchemas.js";
 
@@ -14,11 +13,9 @@ export const listUsers = async (req, res, next) => {
 export const getUserById = async (req, res, next) => {
   try {
     const user = await userModel.getUserById(req.params.id);
-
     if (!user) {
       return res.status(404).json({ error: "Usuario no encontrado" });
     }
-
     res.json(user);
   } catch (err) {
     next(err);
@@ -28,26 +25,17 @@ export const getUserById = async (req, res, next) => {
 export const createUser = async (req, res, next) => {
   try {
     const parsed = userSchema.parse(req.body);
-
     const newUser = await userModel.createUser(parsed);
-
     res.status(201).json(newUser);
   } catch (err) {
-    // error de validación Zod
     if (err.name === "ZodError") {
       return res
         .status(400)
         .json({ error: err.errors.map((e) => e.message).join(", ") });
     }
-
-    // error 409 que lanza el modelo si el email está repetido
     if (err.status === 409) {
       return res.status(409).json({ error: err.message });
     }
-
     next(err);
   }
 };
-
-// opcional: también podrías hacer un export default si quisieras
-// export default { listUsers, getUserById, createUser };
